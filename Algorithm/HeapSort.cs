64e43c1537e8119170baf4ae9600a66e1a025b55
dyman <dyman_zy@163.com>
2017-12-03 11:24:49 +0800

    Add Bucket Sort

--- Algorithm/HeapSort.cs
+++ Algorithm/HeapSort.cs
@@ -7,10 +7,12 @@ namespace Algorithm_CSharp.Algorithm
     public class HeapSort {
         
         public static void Sort(int[] list) {
+            Console.WriteLine("排序前： " + string.Join(",", list));
             //  循环建立初始堆
             for (int i = list.Length / 2; i >= 0; i--) {
                 HeapAdjust(list, i, list.Length);
             }
+            Console.WriteLine("最大堆： " + string.Join(",", list));
 
             //  进行 n-1 次循环，完成排序
             for (int i = list.Length - 1; i > 0; i--) {
@@ -19,11 +21,17 @@ namespace Algorithm_CSharp.Algorithm
                 list[0] = temp;
 
                 HeapAdjust(list, 0, i);
-
+                Console.WriteLine("一次排序之后： " + string.Join(",", list));
             }
         }
 
 
+        /// <summary>
+        /// parent 为当前结点
+        /// 左孩子结点为： 2 * i + 1
+        /// 右孩子结点为： 2 * i + 2
+        /// 父结点为   ： (i-1) / 2
+        /// </summary>
         private static void HeapAdjust(int[] array, int parent, int length) {
             int temp = array[parent];       // temp保存当前父节点
             int child = 2 * parent + 1;     // 先获得左孩子
