28dc586cfa8df6b4aa7a4f3dcf1fad0e5de954c3
Dyman <dyman_zy@163.com>
2017-12-08 18:37:36 +0800

    Add TimSort

---
+++ Algorithm/SimpleTimSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SimpleTimSort
+{
+
+	private static int MIN_MERGE = 16;
+	private int[] a;
+	private int[] aux;
+
+	private int[] runsBase = new int[40];
+	private int[] runsLen = new int[40];
+	private int stackTop = 0;
+
+
+	public static void Sort(int[] data) {
+	}
+
+
+	private void inserSort(int[] data, int from, int to, int n) { }
+
+
+	private int maxAscendingLen(int[] data, int from) { return 1; }
+
+
+	private void pushRun(int baseIndex, int len) { }
+
+
+	private int needMerge() { return -1; }
+
+
+	private int gallopLeft(int[] data, int baseIndex, int len, int value) { return 0; }
+
+
+	private int gallopRight(int[] data, int baseIndex, int len, int value) { return 0; }
+
+
+	private void mergeAt(int x) { }
+
+
+	private void forceMerge() { }
+}
---
+++ Algorithm/TimSort.cs
@@ -0,0 +1,13 @@
+﻿/********************************************************************
+	created:	2017/12/08
+	filename: 	ArraySort.cs
+	author:		dyman
+	
+	purpose:	Java中TimSort()的源码  （)
+*********************************************************************/
+
+
+public class TimSort {
+	public static void Sort(int[] data) {
+	}
+}
